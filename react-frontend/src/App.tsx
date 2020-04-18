e26b52e5611fc1a964029cc60cc0bbf0a0639ea3
Avarka <vadavar7@gmail.com>
2020-04-18 14:52:17 +0200

    Login form changes Main added

---
+++ react-frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import Login from "./Login";
+import Main from "./Main";
+
+const App = () => {
+    const switchToMain = () => {
+        setRender(<Main/>)
+    }
+
+    const [render, setRender] = useState(<Login login={switchToMain}/>);
+
+    return (
+        <>
+            {render}
+        </>
+    );
+};
+
+export default App;
--- react-frontend/src/index.tsx
+++ react-frontend/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/main.css'
-import Login from './Login';
 import * as serviceWorker from './serviceWorker';
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
+      <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
