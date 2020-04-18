4cd4c968ba1e3a3fc1502baddb247b4f4ccb4425
Ruzsa Gergely <ruzsa.gergely@gmail.com>
2020-04-18 15:03:45 +0200

    Users REST endpoint module

---
+++ users_rest_endpoint/src/Plugin/rest/resource/UsersListResource.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\users_rest_endpoint\Plugin\rest\resource;
+
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+
+/**
+ * Provides a Demo Resource
+ *
+ * @RestResource(
+ *   id = "users_list_resource",
+ *   label = @Translation("User List resource"),
+ *   uri_paths = {
+ *     "canonical" = "/api/users_list"
+ *   }
+ * )
+ */
+class UsersListResource extends ResourceBase
+{
+  /**
+   * Responds to entity GET requests.
+   * @return \Drupal\rest\ResourceResponse
+   */
+  public function get()
+  {
+
+    $query = \Drupal::entityQuery('user');
+    $uids = $query->execute();
+
+    $response = Array();
+
+    foreach ($uids as $key => $value) {
+      $account = \Drupal\user\Entity\User::load($value);
+      $name = $account->getDisplayName();
+      $user = \Drupal\user\Entity\User::load($value);
+      $roles = $user->getRoles();
+      
+      array_push($response, Array(
+        "uid" => $key,
+        "username" => $name,
+        "roles" => $roles
+      ));
+
+    }
+    //$response = ['message' => 'Hello, this is a rest service'];
+    return new ResourceResponse($response);
+  }
+}
